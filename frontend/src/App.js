6daff37e047fdccdfd05223aa7aeecc58fc82805
Kiet Vu <kietvu.228@gmail.com>
2022-04-23 14:29:14 +0700

    React setup

---
+++ frontend/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+    return (
+        <React.Fragment>
+            <h1>Welcome to proshop</h1>
+        </React.Fragment>
+    );
+}
+
+export default App;
