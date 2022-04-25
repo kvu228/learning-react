5239ad886c8de3900ebff392420dd3ba223ff848
Kiet Vu <kietvu.228@gmail.com>
2022-04-26 00:08:28 +0700

    Add Rating components

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,24 +1,33 @@
 import React from "react";
+import Rating from "./Rating";
 
 const Product = ({ product }) => {
     return (
         <div className='card my-3 p-3 rounded'>
             <a href={`/product/${product._id}`}>
                 <img
-                    src={product.image}
+                    // src={product.image}
+                    src='/images/airpods.jpg'
                     alt='pro-shop'
                     className='card-img-top'
                 />
             </a>
             <div className='card-body'>
-                <a href={`/product/${product._id}`}>
-                    <h3 className='card-title '>
+                <a
+                    href={`/product/${product._id}`}
+                    className='text-decoration-none'
+                >
+                    <p className='card-title display-7 text-black'>
                         <strong>{product.name}</strong>
-                    </h3>
+                    </p>
                 </a>
-                <div className='card-text'>
+                {/* <div className='card-text'>
                     {product.rating} from {product.numReviews}
-                </div>
+                </div> */}
+                <Rating
+                    value={product.rating}
+                    text={`${product.numReviews} reviews`}
+                />
                 <h3 className='card-text'>
                     <strong>$ {product.price}</strong>
                 </h3>
