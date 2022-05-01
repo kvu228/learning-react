77b6bc8ad212ecdf59c4c0419076c2c0737c28d9
Kiet Vu <kietvu.228@gmail.com>
2022-05-02 00:38:53 +0700

    Adding ProductScreen

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,7 +1,83 @@
 import React from "react";
+import { Link, useParams } from "react-router-dom";
+import {
+    Row,
+    Col,
+    Image,
+    ListGroup,
+    Card,
+    Button,
+    ListGroupItem,
+} from "react-bootstrap";
 
-const ProductScreen = () => {
-    return <div>ProductScreen</div>;
+import Rating from "../components/Rating";
+import products from "../resources/products";
+
+const ProductScreen = ({ match }) => {
+    const productID = useParams().id;
+    const product = products.find((product) => product._id === productID);
+    return (
+        <React.Fragment>
+            <Link className='btn btn-dark my-3' to='/'>
+                Go back
+            </Link>
+            <Row>
+                <Col md={6}>
+                    <Image scr={product.image} alt={product.name} fluid />
+                </Col>
+                <Col md={3}>
+                    <ListGroup variant='flush'>
+                        <ListGroupItem>
+                            <h3>{product.name}</h3>
+                        </ListGroupItem>
+                        <ListGroupItem>
+                            <Rating
+                                value={product.rating}
+                                text={`${product.numReviews} reviews`}
+                            />
+                        </ListGroupItem>
+                        <ListGroupItem>Price: ${product.price}</ListGroupItem>
+                        <ListGroupItem>
+                            Description: {product.description}
+                        </ListGroupItem>
+                    </ListGroup>
+                </Col>
+                <Col md={3}>
+                    <Card>
+                        <ListGroup variant='flush'>
+                            <ListGroupItem>
+                                <Row>
+                                    <Col>Price</Col>
+                                    <Col>
+                                        <strong> ${product.price}</strong>
+                                    </Col>
+                                </Row>
+                            </ListGroupItem>
+                            <ListGroupItem>
+                                <Row>
+                                    <Col>Status</Col>
+                                    <Col>
+                                        {product.countInStock > 0
+                                            ? `${product.countInStock} in stock`
+                                            : "Out of stock"}
+                                    </Col>
+                                </Row>
+                            </ListGroupItem>
+                            <ListGroupItem>
+                                <Button
+                                    className='btn-dark'
+                                    type='button'
+                                    disabled={product.countInStock === 0}
+                                >
+                                    Add to card
+                                </Button>
+                            </ListGroupItem>
+                        </ListGroup>
+                    </Card>
+                </Col>
+            </Row>
+        </React.Fragment>
+    );
 };
 
 export default ProductScreen;
