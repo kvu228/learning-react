0888926efdb8c331f0e9792dc706de37bd77ad61
Kiet Vu <kietvu.228@gmail.com>
2022-05-02 21:13:35 +0700

    install Concurrently, .ENV

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,8 @@
-const express = require("express");
-const products = require("./data/products");
+import express from "express";
+import products from "./data/products.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 
@@ -16,4 +19,9 @@ app.get("/api/product/:id", (req, res) => {
     res.json(product);
 });
 
-app.listen(5000, console.log("Server is running on port 500"));
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+    PORT,
+    console.log(`Server is running ${process.env.NODE_ENV} on port ${PORT}`),
+);
